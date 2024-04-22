9e0762cdba4d869630eb7e6d634b40bf57dcfd6f
cofeek-codes <kormyshev11@mail.ru>
2024-04-22 18:34:19 +0500

    header and auth getting ready

--- resources/views/welcome.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    {{$user->role->title}}
-</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $user = User::find(1);
-    return view('welcome', ['user' => $user]);
+    return view('home');
+});
+
+// auth
+
+Route::controller(AuthController::class)->prefix('auth')->name('auth.')->group(function () {
 });
